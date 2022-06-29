f2c9e17a7b3f5507e93b748af51d6e3752aff23c
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2022-06-29 09:08:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/english-vocabulary/precache-manifest.397e3b1c665a1e8438fd97a535722b19.js"
+  "/english-vocabulary/precache-manifest.a520c627965b3655d782327914a1d9c7.js"
 );
 
 workbox.clientsClaim();
