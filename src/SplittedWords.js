cc6a1b111c1df40a42d0f3da2acd904760d18dbd
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2022-07-06 16:44:29 +0300

    word translation implemented

--- src/SplittedWords.js
+++ src/SplittedWords.js
@@ -3,22 +3,26 @@ import {Table,  Image, Button, Menu } from 'semantic-ui-react'
 import TopMenu from './TopMenu'
 import {Link} from "react-router-dom";
 import axios from 'axios';
+//import { pronounce } from "node-pronounce";
 
 class SplittedWords extends Component {
 
   constructor(props){
     super(props);
+    this.fileInputRef = React.createRef();
     this.state = {
       wordsEl: [],
       words: [],
+      selected: '',
       loaded: false,
-      isModalOpen: false,
+      isModalFoundOpen: false,
+      isModalInputOpen: false,
       sentence: "arched owl Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
     }
   }
 
   componentDidMount(){
-    axios.get('/working.json')
+    axios.get('/words_full.json')
       .then(res => {
         const words = res.data;
         let splitted = this.state.sentence.split(" ");
@@ -31,29 +35,69 @@ class SplittedWords extends Component {
   searchForWord = (item) => {
     let words = this.state.words.slice()
     let newItem = item.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-    let found = words.find(x => x.name.toLowerCase() === newItem);
+    let found = words.find(x => x.name === newItem);
     if (found) {
       this.setState({
         found
-      }, () => this.toggleModal())
+      }, () => this.toggleFoundModal())
     } else {
-      console.log('not found')
+      this.setState({
+        selected: newItem
+      }, () => this.toggleInputModal())
     }
   } 
 
-  toggleModal = () => {
+  toggleFoundModal = () => {
     this.setState({
-      isModalOpen: !this.state.isModalOpen
+      isModalFoundOpen: !this.state.isModalFoundOpen
     })
   }
 
-  render() {
+  toggleInputModal = () => {
+    this.setState({
+      isModalInputOpen: !this.state.isModalInputOpen
+    })
+  }
 
+  changeFile = (e) => {
+    console.log(e);
+    this.setState({
+      filePath: e.target.value
+    })
+  }
+
+  changeTranslation = (e) => {
+    this.setState({
+      translation: e.target.value
+    })
+  }
+
+  changeDefinition = (e) => {
+    this.setState({
+      definition: e.target.value
+    })
+  }
+
+  addToWords = () => {
+    let word = {};
+    word.name = this.state.selected;
+    word.transription = this.state.transcription;
+    word.translation = this.state.translation;
+    word.category = "common";
+    word.id = 1;
+    word.date = new Date();
+    word.definition = this.state.definition;
+    word.image = this.state.filePath;
+    //pronounce(this.state.selected);
+    console.log(word);
+  }
+
+  render() {
     return (     
       <Fragment>
         <div className="content-wrapper">
           <TopMenu></TopMenu>
-          {this.state.isModalOpen ?
+          {this.state.isModalFoundOpen ?
             <div className = "word-modal__overlay">
               <div className="word-modal">
                 <div className="word-modal__image">
@@ -61,8 +105,35 @@ class SplittedWords extends Component {
                 </div>
                 <div className="word-modal__description">
                   <p className="word-modal__title">{this.state.found.name} - {this.state.found.translation}</p>
-                  <p className="word-modal__meaning">{this.state.found.meaning}</p>   
-                  <button onClick={this.toggleModal}>Закрыть</button>               
+                  <p className="word-modal__meaning">{this.state.found.definition}</p>   
+                  <Button primary onClick={this.toggleFoundModal}>Закрыть</Button>              
+                </div>
+              </div>              
+            </div>
+          : null}
+          {this.state.isModalInputOpen ?
+            <div className = "word-modal__overlay">
+              <div className="word-modal">
+                <div className="word-modal__description">
+                  <p className="word-modal__title">{this.state.selected}</p>
+                  <textarea value = {this.state.translation} onChange = {this.changeTranslation} type = "text" className="word-modal__textarea" placeholder="Добавить перевод" />
+                  <textarea value = {this.state.definition} onChange = {this.changeDefinition} type = "text" className="word-modal__textarea" placeholder="Добавить значение" />   
+                  <Button
+                      primary
+                      content="Выбрать картинку"
+                      labelPosition="left"
+                      icon="file"
+                      onClick={() => this.fileInputRef.current.click()}
+                      className="word-modal__file"
+                  />
+                  <input ref={this.fileInputRef}
+                         type="file"
+                         hidden
+                         onChange={this.changeFile} />
+                  <div className="word-modal__buttons">
+                    <Button primary onClick={this.addToWords}>Добавить</Button>
+                    <Button primary onClick={this.toggleInputModal}>Закрыть</Button>                      
+                  </div>
                 </div>
               </div>              
             </div>
